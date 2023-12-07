45e47a5ab3e1a9c72f7228043ceb9502b1aaa336
Ronan Le Martret <ronan.lemartret@iot.bzh>
2023-12-07 10:41:30 +0100

    Add qrcode support
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- lv_conf.h
+++ lv_conf.h
@@ -474,7 +474,7 @@ extern uint32_t custom_tick_get(void);
 
 #define LV_USE_BTNMATRIX  0
 
-#define LV_USE_CANVAS     0
+#define LV_USE_CANVAS     1
 
 #define LV_USE_CHECKBOX   0
 
@@ -648,7 +648,7 @@ extern uint32_t custom_tick_get(void);
 #define LV_USE_GIF 0
 
 /*QR code library*/
-#define LV_USE_QRCODE 0
+#define LV_USE_QRCODE 1
 
 /*FreeType library*/
 #define LV_USE_FREETYPE 0
